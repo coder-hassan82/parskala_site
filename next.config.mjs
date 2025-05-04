4c43c6c8e9550b4785baa97beb50283b49ec9c5a
coder-hassan82 <h.abbasi8213@gmail.com>
2025-05-04 09:30:27 +0330

    update2

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["hlwzkqxingaysobvysci.supabase.co"],
   },
+  // اضافه کردن این تنظیمات جدید
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  // برای صفحاتی که از useSearchParams استفاده می‌کنند
+  skipTrailingSlashRedirect: true,
+  skipMiddlewareUrlNormalize: true,
 };
 
 export default nextConfig;
