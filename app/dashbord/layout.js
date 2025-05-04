4965ffa331ddf1a2476b0fc5a46069ecefb2836c
coder-hassan82 <h.abbasi8213@gmail.com>
2025-05-04 09:24:38 +0330

    upadte

--- app/dashbord/layout.js
+++ app/dashbord/layout.js
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import Navigation from "./Navigation";
 import style from "@/app/_style/DashBord.module.css";
 export default function layout({ children }) {
   return (
     <div className={style.parent}>
       <Navigation />
+      <Suspense fallback={<div>Loading...</div>}>
       <div className={style.div1}>{children}</div>
+      </Suspense>
     </div>
   );
 }
