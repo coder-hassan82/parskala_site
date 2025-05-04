77f8973bad490e6a4d1aea717f21f6ac6db5dc62
coder-hassan82 <h.abbasi8213@gmail.com>
2025-05-04 09:19:36 +0330

    update

--- app/cart/layout.js
+++ app/cart/layout.js
@@ -1,10 +1,14 @@
+// app/cart/layout.js
+import { Suspense } from 'react';
 import Navigation from "./Navigation";
 
 export default function Layout({ children }) {
   return (
     <div>
       <Navigation />
-      {children}
+      <Suspense fallback={<div>Loading...</div>}>
+        {children}
+      </Suspense>
     </div>
   );
 }
